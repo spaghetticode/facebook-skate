1d9296c01b81c0e88357881102278909dda14400
Andrea Longhi <andrea74@mailshack.com>
2010-02-11 16:37:26 +0100

    conditions refactoring

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -5,7 +5,7 @@ class ActivitiesController < ApplicationController
       flash[:error] = "Per oggi ti sei allenato abbastanza, ora sfida un amico o fai qualcos'altro."
       redirect_to matches_path and return
     else
-      @training_session = Message.first(:conditions => ['category = ?', 'training'], :order => 'random()')
+      @training_session = Message.first(:conditions => {:category => 'training'}, :order => 'random()')
       flash[:notice] = 'Ti sei allenato con impegno e hai guadagnato 10 punti esperienza.'
     end
   end
